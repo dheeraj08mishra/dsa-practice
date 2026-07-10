cd7f58054180ecbfe7b601987da27e5dc8b74ddd
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2026-07-10 21:13:07 +0530

    LeetCode: Odd Even Linked List (JS)

---
+++ javascript/linked-list/Odd-Even-Linked-List.js
@@ -0,0 +1,56 @@
+/*
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
+Example 2:
+
+
+Input: head = [2,1,3,5,6,4,7]
+Output: [2,3,6,7,1,5,4]
+*/
+
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+var oddEvenList = function (head) {
+  if (!head) return head;
+  let temp = head;
+  let odd = head;
+  let even = head.next;
+
+  let oddlist = odd;
+  let evenlist = even;
+
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenlist;
+  return oddlist;
+};
+
+console.log(oddEvenList(head));
