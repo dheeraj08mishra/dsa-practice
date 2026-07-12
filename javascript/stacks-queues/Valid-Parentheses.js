54010d8e494f7be6da9aacb54c0600713de2c928
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2026-07-12 14:10:37 +0530

    LeetCode: Valid Parentheses (JS)

---
+++ javascript/stacks-queues/Valid-Parentheses.js
@@ -0,0 +1,54 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Example 4:
+Input: s = "([])"
+Output: true
+
+Example 5:
+Input: s = "([)]"
+Output: false
+*/
+
+var isValid = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (["(", "[", "{"].includes(s[i])) {
+      stack.push(s[i]);
+    } else {
+      let top = stack[stack.length - 1];
+      if (
+        (s[i] === ")" && top === "(") ||
+        (s[i] === "}" && top === "{") ||
+        (s[i] === "]" && top === "[")
+      ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid("()[]{}"));
+console.log(isValid("()"));
+console.log(isValid("(]"));
+console.log(isValid("([])"));
+console.log(isValid("([)]"));
